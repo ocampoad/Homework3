05e630480672032c647d51f15fb793ae6265655f
ocampoad <ocampo.adriane13@gmail.com>
2022-05-11 21:54:45 -0700

    writePassword function

--- script.js
+++ script.js
@@ -14,49 +14,43 @@ function writePassword() {
   const checkupp = document.querySelector("#Uppercase").checked;
   const checknum = document.querySelector("#Numeric").checked;
   const checkspe = document.querySelector("#Special").checked;
-  const yousure = confirm("Are you sure?");
+  const yousure = confirm("Are you certain of your selection?");
   if (!yousure) {
     alert("Please edit your selection to your preference")
   } else {
     if (!checklow && !checkupp && !checknum && !checkspe) {
       alert("Please select at least one character type!")
     } else {
-      alert("it worked");
-      var concatpassword = [""];
-      const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+      let concatpassword = "";
+      const lowletters = "abcdefghijklmnopqrstuvwxyz";
+      const upletters = lowletters.toUpperCase();
+      const numbers = "0123456789"
+      const specialchar = "~!@#$%^&*()_+-=<>?/|{}[]";
+      if (checklow) {
+        concatpassword = concatpassword + lowletters;
+      };
+
+      if (checkupp) {
+        concatpassword = concatpassword + upletters;
+      };
+
+      if (checknum) {
+        concatpassword = concatpassword + numbers;
+      };
+
+      if (checkspe) {
+        concatpassword = concatpassword + specialchar;
+      };
+      let displaypassword = "";
       for (let i = 0; i < passlength; i++) {
-        const whichchar = Math.floor(Math.random() * 4);
-        const randomlower = letters[Math.floor(Math.random()*letters.length)]
-        if (whichchar === 0) {
-          // alert(randomlower)
-          concatpassword = concatpassword + randomlower;
-          // alert(concatpassword);
-        } else if (whichchar === 1) {
-          // alert(randomlower.toUpperCase())
-          concatpassword = concatpassword + randomlower.toUpperCase();
-          // alert(concatpassword);
-        } else if (whichchar === 2) {
-          // alert( Math.floor(Math.random()*10));
-          concatpassword = concatpassword + Math.floor(Math.random()*10)
-          // alert(concatpassword);
-        } else {
-          // alert("some character")
-          concatpassword = concatpassword + "$"
-        }
-       
+        let somerandomnumber = Math.floor(Math.random() * concatpassword.length);
+        displaypassword = displaypassword + concatpassword[somerandomnumber];
       }
-      alert(concatpassword +"!");
+      const passwordText = document.querySelector("#password");
+      passwordText.value = displaypassword;
     }
   }
-
-  // const password = generatePassword(); {
-
-  // }
-
-  // const passwordText = document.querySelector("#password");
-  // passwordText.value = password;
-
-};
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", displaybtn);
@@ -66,11 +60,3 @@ passwordBtn.addEventListener("click", writePassword);
 function passwordlengthfunc(val) {
   document.querySelector("#passwordvalue").innerHTML = val;
 };
-
-// var checklow = document.querySelector("#Lowercase").checked;
-
-// math.round
-
-// else (please select at least one character type!)
-
-// var item = items[Math.floor(Math.random()*items.length)];
